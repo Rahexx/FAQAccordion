29ee04b851ab176cc9463837f9021b89be01cf82
Rahexx <Rahexx@users.noreply.github.com>
2025-06-11 07:59:19 +0000

    deploy: 0e205038003fabd03c8c2c7ecce231e8beb5011b

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 function setCollapseActive(collapseEl, isActive) {
   if (!collapseEl) return;
 
+  const header = collapseEl.querySelector('.collapse__header');
+  const content = collapseEl.querySelector('.collapse__content');
   const icon = collapseEl.querySelector('.collapse__icon');
 
   if (isActive) {
     collapseEl.classList.remove('active');
+    header.setAttribute('aria-expanded', 'false');
+    header.setAttribute('aria-hidden', 'true');
     if (icon) icon.src = './assets/images/icon-plus.svg';
   } else {
     collapseEl.classList.add('active');
+    header.setAttribute('aria-expanded', 'true');
+    header.setAttribute('aria-hidden', 'false');
     if (icon) icon.src = './assets/images/icon-minus.svg';
   }
 }
